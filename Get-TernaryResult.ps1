f662f150fdeb877a21abd59137687ba10bff4179
ddenning0 <ddenning@protonmail.com>
2017-11-26 14:57:01 -0800

    Split the module into individual files so they can easily be used seperately.

--- Get-TernaryResult.ps1
+++ Get-TernaryResult.ps1
@@ -28,7 +28,7 @@ Strings evaluate to true
 
 .EXAMPLE
 Get-TernaryResult "foo" "bar" $null
-$null evaluates to false. If not piped in or named then Conditional must be the last parameter
+$null evaluates to false. If not piped in or named then Condition must be the last parameter
 
 .EXAMPLE
 0 | Get-TernaryResult "foo" "bar"
@@ -53,66 +53,3 @@ function Get-TernaryResult{
     END { return @{$true=$TrueVal;$false=$FalseVal}[[bool]$Condition] }
 }
 New-Alias -Name "?:" -Value Get-TernaryResult -Description "Powershell doesn't have a ternary operator, so use this instead."
-
-
-<#
-.SYNOPSIS
-Convenience function to filter Compare-Object results.
-
-.DESCRIPTION
-Split-Diff will filter the results from Compare-Object and only return results from the given side.
-
-.PARAMETER Obj
-The output from Compare-Object without PassTru set.
-
-.PARAMETER Side
-The 'Side' to return results from.
-
-Left returns results with <= as the SideIndicator
-Right returns results with <= as the SideIndicator
-Both returns results with <= as the SideIndicator
-
-.PARAMETER PassThru
-Same as PassThru for Compare-Object. If set, the result objects themselves are returned, not as the hashtable entry Compare-Object casts them into.
-
-.EXAMPLE
-compare 1,2,3,4 3,4,5,6 | Split-Diff -Side Left
-Returns: InputObject SideIndicator
-         ----------- -------------
-                   1 <=
-                   2 <=
-
-.EXAMPLE
-compare 1,2,3,4 3,4,5,6 -IncludeEqual | Split-Diff -Side Both -PassThru
-Returns: 3, 4
-
-.NOTES
-Just a simple helper function.
-#>
-Filter Split-Diff{
-    Param(
-        [Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)]
-        [ValidateNotNull()]
-        [PSCustomObject[]]$Obj,
-        [Parameter(Mandatory=$true)]
-        [ValidateSet('Left','Right','Both')]
-        [String]$Side,
-        [Switch]$PassThru
-    )
-    PROCESS {
-        if ( $Side -eq 'Left' ){
-             $tmp = $Obj| Where-Object { $_.sideindicator -eq '<=' }
-        }
-        elseif( $Side -eq 'Right' ){
-            $tmp = $Obj| Where-Object { $_.sideindicator -eq '=>' }
-        }
-        elseif( $Side -eq 'Both' ){
-            $tmp = $Obj| Where-Object { $_.sideindicator -eq '==' }
-        }
-        if($PassThru){ return $tmp.InputObject } else { return $tmp }
-    }
-}
-
-
-## EXPORT MEMBERS ##
-Export-ModuleMember -Function *-* -Alias *
--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # dd-psutils
 I use Powershell a lot in my day job.
 
-# usage
+# Usage
 
-Powershell manifests are tools of hegemonic oppression so I don't bother with them.
+1. Dot source the files you want. If script execution is restricted then I feel sorry for you. Stick it to the man and pipe the file contents into the Invoke-Expression cmdlet instead:
 ```
-Import-Module -Name path/to/dd-psutils.psm1
+Get-Content filename.ps1 | Out-String | Invoke-Expression
 ```
+2. Use them. See a function's Get-Help for more information.
